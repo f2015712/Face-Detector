d08f32f5601c765ed926b9c03960d45d0fb735cb
Siddharth Shukla <sks2971996@gmail.com>
2019-02-28 21:52:13 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np 
+
+face_cascade=cv2.CascadeClassifier('C:\opencv_sample\opencv-master\data\haarcascades\haarcascade_frontalface_default.xml')
+videocapture=cv2.VideoCapture(0)
+scale_factor=1.3
+font=cv2.FONT_HERSHEY_DUPLEX
+while  True:
+	ret,pic=videocapture.read()
+	faces=face_cascade.detectMultiScale(pic,scale_factor,5)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(pic,(x,y),(x+w,y+h),(255,0,0),2)
+		cv2.putText(pic,'Me',(x,y),font,2,(255,255,255),2,cv2.LINE_AA)
+	print("Number of faces detected {} ".format(len(faces)))
+	cv2.imshow('Face',pic)
+	k=cv2.waitKey(27) & 0xff
+	if k==2:
+		break
+cv2.DestroyAllWindows()
